f55568063d8687ff836de4697d2a6307c9a3910b
QuCF <dauhalyhi@gmail.com>
2025-06-25 17:48:59 -0700

    qiskit QSVT

--- jupyter-notebooks/Carleman/carleman.py
+++ jupyter-notebooks/Carleman/carleman.py
@@ -289,7 +289,7 @@ def solve_carleman_global(
     # * x_init_cond: initial conditions;
     # * N_nl_emb: the number of NL terms used for Carleman embedding;
     # * N_nl_sys: the highest (assumed) nonlinearity in the original system of equations;
-    # * return_sys_f: function which returns the system organized as F_terms using 
+    # * return_sys: function which returns the system organized as F_terms using 
     #           the system parameters given as a dictionary (map);
     # * zeta_th: the chart size;
     # --------------------------------------------------------------
@@ -460,9 +460,214 @@ def solve_carleman_global(
             else:
                 sol_carleman[Nt_act - 1,:] = shift_x(x_loc, zeta_gl)
 
-        # --- remove empty cells from the resulting lists ---    
-        t_res = t_res[:Nt_act]
-        sol_carleman = sol_carleman[:Nt_act,:]
+    # --- remove empty cells from the resulting lists ---    
+    t_res = t_res[:Nt_act]
+    sol_carleman = sol_carleman[:Nt_act,:]
+    print("Done")
+    return sol_carleman, t_res, "GL"
+
+
+# -----------------------------------------------------------------------------------------------
+# --- Globalised adaptive Carleman embedding of a nonlinear system with Nx variables ---
+def solve_carleman_global_adaptive(
+        x_init_cond, N_nl_emb, N_nl_sys, t, 
+        sys_coefs, return_sys, shift_coefs, 
+        zeta_th_init = 0.6, 
+        error_threshold = 1.e-1,
+        eta_err = 1.e-1,
+        N_iter_max = 100.
+    ):
+    # --------------------------------------------------------------
+    # * x_init_cond: initial conditions;
+    # * N_nl_emb: the number of NL terms used for Carleman embedding;
+    # * N_nl_sys: the highest (assumed) nonlinearity in the original system of equations;
+    # * sys_coefs: dictionary with the initial valus of the coefficients describing the target dynamic system; 
+    # * return_sys: function which returns the dynamic system organized as matrices F_terms using 
+    #           the system parameters given as a dictionary;
+    # * zeta_th_init: the initial chart size;
+    # * error_threshold: if the signal achieves this limit, zeta decreases;
+    # * eta_err: must be 0 < eta_err < 1: this value is multiplied by error_threshold 
+    #           to find the threshold when zeta should be increased to speed up simulations;
+    # * N_iter_max: the maximum number of reductions of zeta;
+    # ---
+    # > In the adaptive procedure, one compares simulations with zeta and zeta/2 and changes zeta according to the observed error.
+    # > Zeta decreases by 2., if |x - x'|/min(x,x') >= error_threshold, 
+    #       where x and x' are computed using zeta and zeta/2., correspondingly.
+    # > Zeta increases by 2., if |x - x'|/min(x,x') <= error_threshold * eta_err;
+    # > Zeta is always kept < 1.
+    # --------------------------------------------------------------
+    def f_to_RK(t, x):
+        f_eqs = np.dot(A_chart, x) + B_chart
+        return f_eqs
+    # --------------------------------------------------------------
+    def prepare_init(x_init_loc):
+        xs_init = np.zeros(N_tot)
+        prod = 1.
+        i_start = 0
+        for i_nl in range(N_nl_emb):
+            i_end = i_start + N_terms[i_nl]
+
+            # *** i_nl-th Kronecker product ***
+            prod = np.kron(x_init_loc, prod)
+
+            # *** set the initial conditions for the terms of the i_nl-th Carleman term ***
+            xs_init[i_start:i_end] = prod[:]
+            i_start = i_end
+        return xs_init
+    # --------------------------------------------------------------
+    def prepare_matrices_AB(F_terms_loc):
+        # *** matrix B ***
+        B = np.zeros(N_tot)
+        B[:N_terms[0]] = F_terms_loc[0][:,0]
+
+        # *** matrix A ***
+        A = np.zeros((N_tot, N_tot))
+        i_start_r = 0
+        for i_nl in range(N_nl_emb):  # -> consider N_nl_emb rows with blocks where
+                # the i_nl-th row contains (non-square) matrices with 
+                # the number of rows = N_terms[i_nl];
+                # -> correspondingly, the submatrix at ir-th block-row and ic-th block-column has
+                # N_terms[ir] rows and N_terms[ic] columns;
+            N_loc = N_terms[i_nl]
+            i_end_r = i_start_r + N_loc
+    
+            # *** diagonal blocks ***
+            i_start_c = i_start_r
+            i_end_c   = i_end_r
+            A[i_start_r:i_end_r, i_start_c:i_end_c] = form_block(0, i_nl, F_terms_loc)
+            del i_start_c, i_end_c
+
+            # *** left-sideband blocks ***
+            if i_nl > 0:
+                i_start_c = i_start_r - N_terms[i_nl - 1]
+                i_end_c   = i_start_c + N_terms[i_nl - 1]
+                A[i_start_r:i_end_r, i_start_c:i_end_c] = form_block(-1, i_nl, F_terms_loc)
+                del i_start_c, i_end_c
+
+            # *** blocks in several right sidebands ***  
+            i_start_c = i_start_r
+            i_end_c = None
+            for jj in range(N_nl_sys - 1): # consider (N_nl_sys - 1) right sidebands
+                jj_sb = jj + 1
+                if i_nl < (N_nl_emb - jj_sb): # the block-rows near the righ-hand side of the matrix does not 
+                            # include one or several right sidebands
+                    i_start_c += N_terms[i_nl + jj]
+                    i_end_c   = i_start_c + N_terms[i_nl + jj_sb]
+                    A[i_start_r:i_end_r, i_start_c:i_end_c] = form_block(jj_sb, i_nl, F_terms_loc)
+            del i_start_c, i_end_c
+
+            # *** shift the starting row ***
+            i_start_r += N_loc
+        return A, B
+    # --------------------------------------------------------------
+    def form_block(jj_sideband, i_nl, F_terms_loc):
+        jj_sideband = int(jj_sideband)
+        Nr, Nc = Nx**(i_nl+1), Nx**(i_nl + 1 + jj_sideband)
+        term_sum = np.zeros((Nr, Nc))
+        for ii_sum in range(i_nl+1):
+            one_left  = eyes_list[ii_sum]
+            one_right = eyes_list[i_nl - ii_sum]
+            temp_prod = np.kron(
+                one_left, 
+                np.kron(F_terms_loc[1 + jj_sideband], one_right)
+            )
+            term_sum += temp_prod
+        return term_sum
+    # --------------------------------------------------------------
+    def shift_x(xs, zeta):
+        xs_new = np.array(xs)
+        for ix in range(Nx):
+            xs_new[ix] = xs_new[ix] + zeta[ix]
+        return xs_new
+    # --------------------------------------------------------------
+    def get_radius(xs):
+        sum_of_squares = np.sum(np.square(xs))
+        r = np.sqrt(sum_of_squares)
+        return r
+    # --------------------------------------------------------------
+    
+    # --- the number of variables ---
+    F_terms = return_sys(sys_coefs)
+    Nx = np.shape(F_terms[0])[0]
+
+    # --- time parameters ---
+    Nt = len(t)
+    dt = np.diff(t)[0]
+    t_res = np.zeros(Nt)
+    t_global = 0.
+
+    # --- The number of elements for each next nonlinear term in embedding ---
+    N_terms = np.zeros(N_nl_emb, dtype=int)
+    N_tot = 0
+    for ii in range(N_nl_emb):
+        N_terms[ii] = int(Nx**(ii+1))
+        N_tot += N_terms[ii]
+    print("The total number of variables in the embedding: {:d}".format(N_tot))
+
+    # --- Prepare unit matrices ---
+    eyes_list = [None] * N_nl_emb
+    for ii in range(N_nl_emb):
+        eyes_list[ii] = np.eye(int(Nx**ii))
+
+    # --- Initialize a matrix to save results ---
+    sol_carleman = np.zeros((Nt, N_terms[0]))
+    Nt_act = 1
+
+    # --- Carleman computation in different charts ---
+    curr_sol = [] # reference simulation for some xi at first 
+    comp_sol_half   = [] # simulation for xi/2.
+    comp_sol_double = []
+    x_init_curr = np.array(x_init_cond)
+
+    # might need to set zeta_gl = x_init_curr
+    # and then set x_init_curr = 0
+
+    zeta_gl = np.zeros(Nx)
+    while t_global < t[-1]:
+        # --- Form the system: d_t u = A u + B ---
+        A_chart, B_chart = prepare_matrices_AB(
+            return_sys(shift_coefs(sys_coefs, zeta_gl)) 
+        )
+        xs_init_sys = prepare_init(x_init_curr)
+        curr_sol = shift_x(xs_init_sys[:N_terms[0]], zeta_gl)  
+
+        sol_carleman[Nt_act - 1,:] = curr_sol
+
+        # --- Runge-Kutta solver ---
+        oo = RK45(f_to_RK, t_global, xs_init_sys, t[-1], max_step=dt)
+        while mix.compare_two_strings(oo.status, "running"):
+            oo.step()
+            Nt_act += 1
+            if (Nt_act - 1) >= len(t_res):
+                print("\nWARNING: increase arrays: counter_t = {:d}".format(Nt_act - 1))
+                t_res = np.pad(t_res, (0, Nt), 'constant')
+                sol_carleman = np.pad(sol_carleman, ((0, Nt), (0, 0)), mode='constant')
+
+            t_res[Nt_act - 1] = oo.t
+            t_global          = t_res[Nt_act - 1]
+
+            x_loc = np.array(oo.y[:N_terms[0]])
+            rr = get_radius(x_loc)
+            if rr >= zeta_th_init:
+                # shift the chart;
+                # set the current location as the center of a new chart:
+                zeta_gl    += x_loc
+                x_init_curr = np.zeros(Nx)
+
+                print("--- Shift to another chart, t = {:0.3f} ---".format(t_global))
+                print("radius: {:0.1f}".format(rr))
+                print("zeta_gl: ", end="")
+                mix.print_array(zeta_gl, ff = [6, 2,"f"])
+                print("xs: ", end="")
+                mix.print_array(x_loc, ff = [14, 3, "e"])
+                
+                break
+            else:
+                sol_carleman[Nt_act - 1,:] = shift_x(x_loc, zeta_gl)
+
+    # --- remove empty cells from the resulting lists (CHANGED) ---    
+    t_res = t_res[:Nt_act]
+    sol_carleman = sol_carleman[:Nt_act,:]
     print("Done")
     return sol_carleman, t_res, "GL"
 
@@ -583,64 +788,4 @@ def compare_trajectory_cl_and_carleman(
     ax_loc.tick_params(axis='both', which='major', labelsize=fontsize)
 
     plt.tight_layout()
-    return
-
-
-
-
-
-
-# -----------------------------------------------------------------------------------------------
-# --- Compare classical with Carleman simulations  ---
-def compare_trajectory_cl_and_carleman_PREV(t_ref, sol_ref, t_emb, sol_emb, fontsize = 20, fig_size = (10,9)):
-    fontsize_leg = int(fontsize/4. * 3.)
-
-    # --- classical results ---
-    x_cl = sol_ref[:,0]
-    y_cl = sol_ref[:,1]
-
-    # --- Carleman results ---
-    x_emb = sol_emb[:,0]
-    y_emb = sol_emb[:,1]
-
-    # --- plotting: trajectories ---
-    fig, axs = plt.subplots(2, 1, figsize=fig_size)
-    axs[0].plot(
-        x_cl, y_cl, 
-        color="b", linewidth = 2, linestyle='-', label = "CL"
-    )
-    axs[0].plot(
-        x_emb, y_emb, 
-        color="r", linewidth = 2, linestyle="--", label = "CA"
-    )
-    axs[0].set_xlabel("$x$", fontsize = fontsize)
-    axs[0].set_ylabel("$y$", fontsize = fontsize)
-    offset_text = axs[0].yaxis.get_offset_text()
-    offset_text.set_fontsize(fontsize) 
-
-    axs[0].legend(fontsize = fontsize_leg)
-    axs[0].grid(True)
-    axs[0].tick_params(axis='both', which='major', labelsize=fontsize)
-    # axs[0].set_xticklabels([])
-
-    # --- difference ---
-    x_emb_int = np.interp(t_ref, t_emb, x_emb)
-    y_emb_int = np.interp(t_ref, t_emb, y_emb)
-
-    axs[1].semilogy(
-        t_ref, abs(x_cl - x_emb_int), 
-        color="b", linewidth = 2, linestyle='-', label = "error_x"
-    )
-    axs[1].semilogy(
-        t_ref, abs(y_cl - y_emb_int), 
-        color="r", linewidth = 2, linestyle='-', label = "error_y"
-    )
-    axs[1].set_xlabel('$t$', fontsize = fontsize)
-    axs[1].set_ylabel("$|CL - CA|$", fontsize = fontsize)
-    offset_text = axs[1].yaxis.get_offset_text()
-    offset_text.set_fontsize(fontsize) 
-
-    # axs[1].legend(fontsize = fontsize_leg)
-    axs[1].grid(True)
-    axs[1].tick_params(axis='both', which='major', labelsize=fontsize)
     return
