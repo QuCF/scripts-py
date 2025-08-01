bd5c259792f9677c9201d5118936f0d41167a628
QuCF <dauhalyhi@gmail.com>
2025-08-01 14:55:18 -0700

    AE

--- jupyter-notebooks/Carleman/carleman.py
+++ jupyter-notebooks/Carleman/carleman.py
@@ -288,6 +288,217 @@ def solve_carleman_orig(x_init, N_nl_emb, N_nl_sys, t, F_terms):
 
 
 # -----------------------------------------------------------------------------------------------
+# --- Fixed piecewise non-adaptive Carleman embedding of a nonlinear system with Nx variables ---
+# -----------------------------------------------------------------------------------------------
+def solve_carleman_global_fixed(
+        x_init_cond, N_nl_emb, N_nl_sys, t, 
+        sys_coefs, return_sys, shift_coefs, 
+        zeta_th = 0.1,
+        flag_before_boundary = True
+    ):
+    # --------------------------------------------------------------
+    # * x_init_cond: initial conditions;
+    # * N_nl_emb: the number of NL terms used for Carleman embedding;
+    # * N_nl_sys: the highest (assumed) nonlinearity in the original system of equations;
+    # * return_sys: function which returns the system organized as F_terms using 
+    #           the system parameters given as a dictionary (map);
+    # * zeta_th: the chart size;
+    # --------------------------------------------------------------
+    def f_to_RK(t, x):
+        f_eqs = np.dot(A_chart, x) + B_chart
+        return f_eqs
+    # --------------------------------------------------------------
+    def prepare_init(x_init_loc):
+        xs_init = np.zeros(N_tot)
+        prod = 1.
+        i_start = 0
+        for i_nl in range(N_nl_emb):
+            i_end = i_start + N_terms[i_nl]
+
+            # *** i_nl-th Kronecker product ***
+            prod = np.kron(x_init_loc, prod)
+
+            # *** set the initial conditions for the terms of the i_nl-th Carleman term ***
+            xs_init[i_start:i_end] = prod[:]
+            i_start = i_end
+        return xs_init
+    # --------------------------------------------------------------
+    def prepare_matrices_AB(F_terms_loc):
+        # *** matrix B ***
+        B = np.zeros(N_tot)
+        B[:N_terms[0]] = F_terms_loc[0][:,0]
+
+        # *** matrix A ***
+        A = np.zeros((N_tot, N_tot))
+        i_start_r = 0
+        for i_nl in range(N_nl_emb):  # -> consider N_nl_emb rows with blocks where
+                # the i_nl-th row contains (non-square) matrices with 
+                # the number of rows = N_terms[i_nl];
+                # -> correspondingly, the submatrix at ir-th block-row and ic-th block-column has
+                # N_terms[ir] rows and N_terms[ic] columns;
+            N_loc = N_terms[i_nl]
+            i_end_r = i_start_r + N_loc
+    
+            # *** diagonal blocks ***
+            i_start_c = i_start_r
+            i_end_c   = i_end_r
+            A[i_start_r:i_end_r, i_start_c:i_end_c] = form_block(0, i_nl, F_terms_loc)
+            del i_start_c, i_end_c
+
+            # *** left-sideband blocks ***
+            if i_nl > 0:
+                i_start_c = i_start_r - N_terms[i_nl - 1]
+                i_end_c   = i_start_c + N_terms[i_nl - 1]
+                A[i_start_r:i_end_r, i_start_c:i_end_c] = form_block(-1, i_nl, F_terms_loc)
+                del i_start_c, i_end_c
+
+            # *** blocks in several right sidebands ***  
+            i_start_c = i_start_r
+            i_end_c = None
+            for jj in range(N_nl_sys - 1): # consider (N_nl_sys - 1) right sidebands
+                jj_sb = jj + 1
+                if i_nl < (N_nl_emb - jj_sb): # the block-rows near the righ-hand side of the matrix does not 
+                            # include one or several right sidebands
+                    i_start_c += N_terms[i_nl + jj]
+                    i_end_c   = i_start_c + N_terms[i_nl + jj_sb]
+                    A[i_start_r:i_end_r, i_start_c:i_end_c] = form_block(jj_sb, i_nl, F_terms_loc)
+            del i_start_c, i_end_c
+
+            # *** shift the starting row ***
+            i_start_r += N_loc
+        return A, B
+    # --------------------------------------------------------------
+    def form_block(jj_sideband, i_nl, F_terms_loc):
+        jj_sideband = int(jj_sideband)
+        Nr, Nc = Nx**(i_nl+1), Nx**(i_nl + 1 + jj_sideband)
+        term_sum = np.zeros((Nr, Nc))
+        for ii_sum in range(i_nl+1):
+            one_left  = eyes_list[ii_sum]
+            one_right = eyes_list[i_nl - ii_sum]
+            temp_prod = np.kron(
+                one_left, 
+                np.kron(F_terms_loc[1 + jj_sideband], one_right)
+            )
+            term_sum += temp_prod
+        return term_sum
+    # --------------------------------------------------------------
+    def shift_x(xs, zeta):
+        xs_new = np.array(xs)
+        for ix in range(Nx):
+            xs_new[ix] = xs_new[ix] + zeta[ix]
+        return xs_new
+    # --------------------------------------------------------------
+    def get_radius(xs):
+        sum_of_squares = np.sum(np.square(xs))
+        r = np.sqrt(sum_of_squares)
+        return r
+    # --------------------------------------------------------------
+    
+    # --- the number of variables ---
+    F_terms = return_sys(sys_coefs)
+    Nx = np.shape(F_terms[0])[0]
+
+    # --- time parameters ---
+    Nt = len(t)
+    dt = np.diff(t)[0]
+    
+    # --- The number of elements for each next nonlinear term in embedding ---
+    N_terms = np.zeros(N_nl_emb, dtype=int)
+    N_tot = 0
+    for ii in range(N_nl_emb):
+        N_terms[ii] = int(Nx**(ii+1))
+        N_tot += N_terms[ii]
+    print("The total number of variables in the embedding: {:d}".format(N_tot))
+
+    # --- Prepare unit matrices ---
+    eyes_list = [None] * N_nl_emb
+    for ii in range(N_nl_emb):
+        eyes_list[ii] = np.eye(int(Nx**ii))
+
+    # --- Initialize arrays to save results ---
+    t_res        = np.zeros(Nt)
+    sol_carleman = np.zeros((Nt, N_terms[0]))
+
+    # --- Carleman computation in different charts ---
+    if flag_before_boundary:
+        # Save states only before the horizon was hit: a slower version, higher precision.
+        # Potenially, can stuck in an infinite loop if xi is too small 
+        # (to avoid this, counter_beyond is used).
+        zeta_gl  = np.array(x_init_cond) 
+        t_global = 0.
+        x_prev   = np.zeros(len(x_init_cond)) 
+        Nt_act   = 0
+        t_res[Nt_act]          = t_global
+        sol_carleman[Nt_act,:] = np.array(x_init_cond) 
+        counter_beyond = 0
+        while t_global < t[-1]:
+            A_chart, B_chart = prepare_matrices_AB(
+                return_sys(shift_coefs(sys_coefs, zeta_gl)) 
+            )
+            xs_init_sys = prepare_init(np.zeros(Nx))
+            oo = RK45(f_to_RK, t_global, xs_init_sys, t[-1], max_step=dt)
+            while mix.compare_two_strings(oo.status, "running"):
+                oo.step()
+                x_loc = np.array(oo.y[:N_terms[0]])
+                rr    = get_radius(x_loc)
+                if rr >= zeta_th:
+                    counter_beyond += 1
+                    if counter_beyond > 4:
+                        print("Error: linearization radius is too small")
+                        return None, None, None
+
+                    zeta_gl += x_prev # set the current location as a near-center of a new chart
+                    break
+                else:
+                    counter_beyond = 0
+                    Nt_act += 1
+                    t_global = oo.t
+                    if Nt_act >= len(t_res):
+                        print("\nWARNING: increase arrays: t, counter_t: {:0.3e}, {:d}".format(t_global, Nt_act))
+                        t_res        = np.pad(t_res, (0, Nt), 'constant')
+                        sol_carleman = np.pad(sol_carleman, ((0, Nt), (0, 0)), mode='constant')
+                    t_res[Nt_act]          = t_global
+                    sol_carleman[Nt_act,:] = shift_x(x_loc, zeta_gl)
+                    x_prev = x_loc
+    else:
+        # Save the first state after the horizon: a faster version, but with lower precision
+        zeta_gl  = np.array(x_init_cond) 
+        t_global = 0.
+        Nt_act   = 0
+        t_res[Nt_act]          = t_global
+        sol_carleman[Nt_act,:] = np.array(x_init_cond) 
+        while t_global < t[-1]:
+            A_chart, B_chart = prepare_matrices_AB(
+                return_sys(shift_coefs(sys_coefs, zeta_gl)) 
+            )
+            xs_init_sys = prepare_init(np.zeros(Nx))
+            oo = RK45(f_to_RK, t_global, xs_init_sys, t[-1], max_step=dt)
+            while mix.compare_two_strings(oo.status, "running"):
+                oo.step()
+                t_global = oo.t
+                x_loc = np.array(oo.y[:N_terms[0]])
+
+                Nt_act += 1
+                if Nt_act >= len(t_res):
+                    print("\nWARNING: increase arrays: t, counter_t: {:0.3e}, {:d}".format(t_global, Nt_act))
+                    t_res        = np.pad(t_res, (0, Nt), 'constant')
+                    sol_carleman = np.pad(sol_carleman, ((0, Nt), (0, 0)), mode='constant')
+                t_res[Nt_act]          = t_global
+                sol_carleman[Nt_act,:] = shift_x(x_loc, zeta_gl)
+                
+                rr    = get_radius(x_loc)
+                if rr >= zeta_th:
+                    zeta_gl += x_loc # set the current location as a near-center of a new chart
+                    break
+                
+    # --- remove empty cells from the resulting lists ---    
+    t_res = t_res[:(Nt_act+1)]
+    sol_carleman = sol_carleman[:(Nt_act+1),:]
+    print("Done")
+    return sol_carleman, t_res, "GL"
+
+
+# -----------------------------------------------------------------------------------------------
 # --- Piecewise non-adaptive Carleman embedding of a nonlinear system with Nx variables ---
 # -----------------------------------------------------------------------------------------------
 def solve_carleman_global(
